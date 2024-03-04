d3aa1dd6cb2df0ff4b636b38be0872be5b28cbf6
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-03-04 11:00:19 +0000

    backdrop added

--- menu.js
+++ menu.js
@@ -1,23 +1,44 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const backdropRef = document.querySelector(".backdrop");
 
     mobileMenuRef.classList.add("is-hidden");
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+    function openMenu() {
+        menuBtnRef.classList.add("is-open");
+        menuBtnRef.setAttribute("aria-expanded", "true");
+        mobileMenuRef.classList.remove("is-hidden");
+        backdropRef.classList.add("is-open");
+    }
 
-        mobileMenuRef.classList.toggle("is-hidden", expanded);
+    function closeMenu() {
+        menuBtnRef.classList.remove("is-open");
+        menuBtnRef.setAttribute("aria-expanded", "false");
+        mobileMenuRef.classList.add("is-hidden");
+        backdropRef.classList.remove("is-open");
+    }
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        if (expanded) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
     });
 
     const closeMenuBtnRef = document.querySelector(".close-menu-btn");
     closeMenuBtnRef.addEventListener("click", () => {
-        menuBtnRef.classList.remove("is-open");
-        menuBtnRef.setAttribute("aria-expanded", "false");
-        mobileMenuRef.classList.add("is-hidden");
+        closeMenu();
     });
+
+    const backdropClickHandler = (event) => {
+        if (event.target === backdropRef) {
+            closeMenu();
+        }
+    };
+
+    backdropRef.addEventListener("click", backdropClickHandler);
 })();
